237609d0647bb2547c0ac5f7ec971d2f6fc49c63
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-04-17 21:32:30 -0400

    added code to html

--- assets/js/curency.js
+++ assets/js/curency.js
@@ -1,15 +1,11 @@
 function bringData(event){
     event.preventDefault()//prevent default refresh of submission
-        
     let from=event.target.from.value
     let to=event.target.to.value
     let amount=event.target.amount.value
-      
-        
     if(from==to){
-    document.getElementById("result").innerHTML="please select different currencies" 
+    document.getElementById("result").innerHTML="please select different currencies"
     return}
-    
         const host = 'api.frankfurter.app';
         fetch(`https://${host}/latest?amount=${amount}&from=${from}&to=${to}`)
           .then(resp => resp.json())
